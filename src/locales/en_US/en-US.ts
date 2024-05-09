05c042675f8676d2d4d2a6dfe0c2062dacb5076b
Danilo Valério <danvalerio89@gmail.com>
2024-05-09 15:54:09 -0300

    feat: Add i18next type declaration and update translations for en_US and pt_BR locales

---
+++ src/locales/en_US/en-US.ts
@@ -0,0 +1,42 @@
+export const en_US = {
+  translation: {
+    header: {
+      title: "Smart Contract Deployment in 5 Minutes",
+      subtitle: "Effortless and secure automation for web3 DevOps",
+      button: "Request access",
+    },
+    secondSection: {
+      title: "Redefine Smart Contract Deployment",
+      subtitle:
+        "AnyFlow empowers tech leads and solo web3 developers to achieve agile smart contract deployment.",
+    },
+    features: {
+      continuousIntegration: {
+        title: "Continuous Integration Made Easy",
+        description:
+          "Integrate AnyFlow with your CI tools like GitHub Actions or Jenkins for seamless smart contract deployments. Automate the tedious parts of deployment and focus more on innovation and less on infrastructure.",
+      },
+      handsOffDeployment: {
+        title: "Hands-Off Deployment Process",
+        description:
+          "Forget about gas calculations and private key management. AnyFlow automates these complexities, allowing you to deploy without directly handling sensitive elements or incurring unexpected costs.",
+      },
+      trustAndControl: {
+        title: "Trust and Control in Your Hands",
+        description:
+          "Every deployment through AnyFlow requires explicit approval from your team, ensuring a trustless operation where only authorized actions are executed, and maintaining the integrity and security of your deployments.",
+      },
+      unifiedCosts: {
+        title: "Unified Operational Costs",
+        description:
+          "Simplify your billing with AnyFlow’s straightforward financial model. All deployment costs are consolidated and billed in USD, freeing you from managing multiple native tokens across blockchains.",
+      },
+    },
+    footer: {
+      backedBy: {
+        text: "Backed by Khiza",
+        logo: "Khiza logo URL",
+      },
+    },
+  },
+};
