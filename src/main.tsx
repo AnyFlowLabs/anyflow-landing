227bcbdcdd10a540392680959cb84b24c6a91801
Danilo Valério <danvalerio89@gmail.com>
2024-05-09 16:46:57 -0300

    feat: Update i18n import in main.tsx and remove LanguageChange component from Home.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/global.css";
-import '@/locales/i18n';
+import "@/plugins/i18n.ts";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,16 @@
-import Container from "@/components/organisms/container";
+import { BackgroundAnimated } from "@/components/molecules/background-animated";
+import Container from "@/components/molecules/container";
 import FooterTemplate from "@/components/template/footer-template";
 import HeaderTemplate from "@/components/template/header-template";
 import MainTemplate from "@/components/template/main-template";
-import LanguageChange from "@/components/ui/LanguageChange";
 
 export default function HomePage() {
   return (
     <Container>
-      <div className="bg"></div>
-      <div className="bg bg2"></div>
-      <div className="bg bg3"></div>
+      <BackgroundAnimated />
       <HeaderTemplate />
       <MainTemplate />
       <FooterTemplate />
-      <LanguageChange />
     </Container>
   );
 }
