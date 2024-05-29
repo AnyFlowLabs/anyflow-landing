da287dd97d68ddf0c1fe9ba16eb524772ccc3171
Danilo Valério <danvalerio89@gmail.com>
2024-05-29 10:50:57 -0300

    refactor: Update social media links and try-it-now section styling

--- src/components/template/try-it-now-section.tsx
+++ src/components/template/try-it-now-section.tsx
@@ -1,7 +1,7 @@
 import CallToAction from "@/components/molecules/call-to-action";
 
 const TryItNowSection = () => (
-  <div className="bg-blue-0 mx-auto mt-[82px] flex w-full max-w-[1040px] flex-col items-center gap-6 rounded-[20px] py-[62px]">
+  <div className="mx-auto mt-[82px] flex w-full max-w-[1040px] flex-col items-center gap-6 rounded-[20px] bg-blue-0 py-[62px] text-center lg:text-left">
     <CallToAction />
   </div>
 );
