5e5635b27deddf4ff6257ed45338bc9d1616086e
Yudi Kubota <yudikubota@gmail.com>
2024-10-18 15:31:01 -0300

    add intercom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import Root from "@/components/molecules/root";
 
 import HomePage from "./pages/Home";
+import Intercom from "@intercom/messenger-js-sdk";
 
 export default function App() {
+  Intercom({
+    app_id: 'ovcuu4sd',
+  });
+
   return (
     <Root>
       <HomePage />
