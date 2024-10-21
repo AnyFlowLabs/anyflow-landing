5d4cc203f6345abfaec41c84f56c97c66eaa823a
Danilo Valério <danvalerio89@gmail.com>
2024-10-21 13:42:39 -0300

    WIP: pagination

--- src/pages/ChainDetails.tsx
+++ src/pages/ChainDetails.tsx
@@ -11,12 +11,21 @@ import {
 } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import { CHAINS_API } from "@/const/chainsApi";
+import Pagination from "@/components/pagination/Pagination";
+import { useState } from "react";
+
+const ITEMS_PER_PAGE = 5;
 export default function ChainDetails() {
   const { chainId } = useParams();
-
+  const [currentPage, setCurrentPage] = useState(1);
   const data = CHAINS_API.data.find((chain) => chain.id === Number(chainId));
+  const totalPages = Math.ceil(data!.providers.length / 5) || 1;
 
-  console.log(data);
+  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  const paginatedItems = data!.providers.slice(
+    startIndex,
+    startIndex + ITEMS_PER_PAGE,
+  );
 
   return (
     <Container maxW={"container.xl"} my={5}>
@@ -32,17 +41,21 @@ export default function ChainDetails() {
             </Tr>
           </Thead>
           <Tbody>
-            {data &&
-              data.providers.map((provider) => (
-                <Tr key={provider.id}>
-                  <Td>{provider.name}</Td>
-                  <Td>{provider.id}</Td>
-                  <Td isNumeric>{provider.status}</Td>
-                </Tr>
-              ))}
+            {paginatedItems.map((provider) => (
+              <Tr key={provider.id}>
+                <Td>{provider.name}</Td>
+                <Td>{provider.id}</Td>
+                <Td isNumeric>{provider.status}</Td>
+              </Tr>
+            ))}
           </Tbody>
         </Table>
       </TableContainer>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={setCurrentPage}
+      />
     </Container>
   );
 }
