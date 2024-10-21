d67b20e3e7562f9cb5f37b01ae700dba1c5af000
Danilo Valério <danvalerio89@gmail.com>
2024-10-21 11:45:59 -0300

    chore: add providers and new page

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/global.css";
 import "@/plugins/i18n.ts";
-import { ChakraProvider } from "@chakra-ui/react";
-import customTheme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider theme={customTheme}>
-      <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>,
 );
---
+++ src/pages/Chains.tsx
@@ -0,0 +1,7 @@
+export default function Chains() {
+  return (
+    <div>
+      <h1>Chains</h1>
+    </div>
+  );
+}
