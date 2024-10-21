6f99c3f5b0f24368e577e16ac90aff66dd365288
Danilo Valério <danvalerio89@gmail.com>
2024-10-21 12:30:12 -0300

    chore: add 2 new pages

--- src/pages/Chains.tsx
+++ src/pages/Chains.tsx
@@ -1,7 +1,55 @@
+import { CHAINS_API } from "@/const/chainsApi";
+import {
+  Box,
+  Container,
+  Heading,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+
 export default function Chains() {
+  const navigate = useNavigate();
+
+  const handleChainDetails = (chainName: string) => {
+    navigate(`/chains/${chainName.toLocaleLowerCase()}`);
+  };
+
   return (
-    <div>
-      <h1>Chains</h1>
-    </div>
+    <Container maxW={"container.xl"} my={5}>
+      <Box>
+        <Heading>Chainlist</Heading>
+      </Box>
+
+      <TableContainer>
+        <Table variant="striped">
+          <Thead>
+            <Tr>
+              <Th>Name</Th>
+              <Th>ID</Th>
+              <Th isNumeric>TICKER</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {CHAINS_API.data.map((chain) => (
+              <Tr
+                key={chain.id}
+                cursor="pointer"
+                onClick={() => handleChainDetails(chain.name)}
+              >
+                <Td>{chain.name}</Td>
+                <Td>{chain.id}</Td>
+                <Td isNumeric>{chain.ticker}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 }
