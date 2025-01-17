058b9865ae4df494191b0167ae7b940ae5b0b6e1
William Novak <williamnvk@gmail.com>
2025-01-17 12:15:08 -0300

    feat: enhance RewardsPage with TitleSection component and improved layout
    - Refactored the RewardsPage to include a new TitleSection component for better presentation of key information.
    - Added a 'pre' prop to TitleSection for optional introductory text, enhancing the context of the main title.
    - Updated the layout to replace static text with the TitleSection, improving readability and visual appeal.
    - Introduced new styling for the TitleSection, including gradient text and responsive font sizes.
    
    This commit significantly improves the structure and aesthetics of the RewardsPage, making it more engaging and informative for users.

--- src/pages/Rewards/Components.tsx
+++ src/pages/Rewards/Components.tsx
@@ -1,17 +1,30 @@
 import { VStack, Text, Heading } from "@chakra-ui/react";
 import { FC } from "react";
 
-export const TitleSection: FC<{ title: string; description?: string }> = ({
-  title,
-  description,
-}) => {
+export const TitleSection: FC<{
+  pre?: string;
+  title: string;
+  description?: string;
+}> = ({ pre, title, description }) => {
   return (
-    <VStack>
-      <Heading as="h2" textStyle="title" fontSize={{ base: "2xl", md: "3xl" }}>
+    <VStack gap={{ base: 0, md: 0 }} w="full" align="center" justify="center">
+      {pre && (
+        <Text
+          fontSize="xl"
+          bgGradient="linear(to-r, info.400, success.600)"
+          bgClip="text"
+          fontWeight="bold"
+          fontFamily="heading"
+          letterSpacing={0.5}
+        >
+          {pre}
+        </Text>
+      )}
+      <Heading as="h2" textStyle="title" fontSize={{ base: "2xl", md: "5xl" }}>
         {title}
       </Heading>
       {description && (
-        <Text fontSize="lg" color="gray.600">
+        <Text fontSize="xl" color="gray.200">
           {description}
         </Text>
       )}
--- src/pages/Rewards/RewardsPage.tsx
+++ src/pages/Rewards/RewardsPage.tsx
@@ -9,6 +9,7 @@ import {
   SimpleGrid,
   Text,
   VStack,
+  Link,
 } from "@chakra-ui/react";
 import {
   BoltIcon,
@@ -26,6 +27,11 @@ import {
   WalletIcon,
   LayersIcon,
   MessageSquareIcon,
+  XIcon,
+  GithubIcon,
+  AlertCircleIcon,
+  CheckIcon,
+  TrophyIcon,
 } from "lucide-react";
 import BackedSection from "./Backted";
 import FeaturedSection from "./Featured";
@@ -33,6 +39,7 @@ import ModalClose from "./ModelClose";
 import { RetroGrid } from "./Bg";
 import CardsSection from "./CardsSection";
 import VercelLikeSection from "./VercelLikeSection";
+import { TitleSection } from "./Components";
 
 const RewardsPage = () => {
   return (
@@ -219,12 +226,6 @@ const RewardsPage = () => {
           >
             <Box
               bg="rgba(0,0,0,0.1)"
-              // animation="bounce2 16s cubic-bezier(0, 0, 0.2, 1) infinite"
-              // p={4}
-              // borderRadius="2xl"
-              // border="solid 2px"
-              // borderColor="rgba(0,0,0,0.1)"
-              // backdropFilter="blur(10px)"
               data-aos="flip-right"
               data-aos-delay={100}
             >
@@ -460,28 +461,11 @@ const RewardsPage = () => {
 
       <Container maxW="container.xl" py={{ base: 8, md: 16 }}>
         <VStack spacing={{ base: 1, md: 2 }} align="center" w="full">
-          <Text
-            fontSize="xl"
-            bgGradient="linear(to-r, info.400, success.600)"
-            bgClip="text"
-            fontWeight="bold"
-            fontFamily="heading"
-            letterSpacing={0.5}
-          >
-            Benefits
-          </Text>
-
-          <Heading
-            fontSize={{ base: "3xl", md: "4xl" }}
-            textAlign="center"
-            color="white"
-          >
-            Why join our program?
-          </Heading>
-
-          <Text fontSize="lg" color="gray.50" maxW="2xl" textAlign="center">
-            Get rewarded while helping shape the future of web3 deployment
-          </Text>
+          <TitleSection
+            pre="Benefits"
+            title="Why join our program?"
+            description="Get rewarded while helping shape the future of web3 deployment"
+          />
 
           <SimpleGrid columns={{ base: 1, md: 3 }} spacing={8} w="full" pt={8}>
             <VStack
@@ -603,6 +587,250 @@ const RewardsPage = () => {
           >
             Start earning now
           </Button>
+        </VStack>
+      </Container>
+
+      <Container maxW="container.xl" py={{ base: 8, md: 16 }}>
+        <VStack spacing={{ base: 4, md: 8 }} align="center" w="full">
+          <TitleSection
+            pre="How it works"
+            title="The smart way to deploy smart contracts"
+            description="Stop wasting time and money on outdated methods"
+          />
+
+          <SimpleGrid columns={{ base: 1, md: 3 }} spacing={8} w="full" pt={8}>
+            <VStack
+              p={{ base: 6, md: 16 }}
+              borderRadius="lg"
+              border="2px solid"
+              borderColor="gray.600"
+              align="center"
+              justify="center"
+              spacing={{ base: 2, md: 4 }}
+            >
+              <Heading size="md" color="gray.100" textAlign="left" w="full">
+                Traditional deployment
+              </Heading>
+              <Text color="danger.300" textAlign="left" w="full">
+                The slow, risky and boring way
+              </Text>
+
+              <VStack align="start" spacing={4} w="full">
+                <HStack align="start" justify="center" w="full">
+                  <XIcon size={20} color="var(--anyflow-colors-danger-300)" />
+                  <VStack align="start" gap={0} flex={1}>
+                    <Text color="gray.300">Complex setup process</Text>
+                    <Text color="gray.400" fontSize="sm">
+                      Wastes 2-3 hours on configuration, faucets, funding,
+                      verification…
+                    </Text>
+                  </VStack>
+                </HStack>
+
+                <HStack align="start" justify="center" w="full">
+                  <XIcon size={20} color="var(--anyflow-colors-danger-300)" />
+                  <VStack align="start" gap={0} flex={1}>
+                    <Text color="gray.300">High risk</Text>
+                    <Text color="gray.400" fontSize="sm">
+                      Holds private keys on a possibly compromised machine
+                    </Text>
+                  </VStack>
+                </HStack>
+
+                <HStack align="start" justify="center" w="full">
+                  <XIcon size={20} color="var(--anyflow-colors-danger-300)" />
+                  <VStack align="start" gap={0} flex={1}>
+                    <Text color="gray.300">Isolated experience</Text>
+                    <Text color="gray.400" fontSize="sm">
+                      No community support
+                    </Text>
+                  </VStack>
+                </HStack>
+
+                <HStack align="start" justify="center" w="full">
+                  <XIcon size={20} color="var(--anyflow-colors-danger-300)" />
+                  <VStack align="start" gap={0} flex={1}>
+                    <Text color="gray.300">Limited chain support</Text>
+                    <Text color="gray.400" fontSize="sm">
+                      One chain at a time
+                    </Text>
+                  </VStack>
+                </HStack>
+              </VStack>
+            </VStack>
+
+            <VStack
+              p={{ base: 6, md: 6 }}
+              my={{ base: 0, md: -8 }}
+              bgGradient="linear(to-br, brand.500, transparent, transparent, transparent, transparent)"
+              borderRadius="lg"
+              border="4px solid"
+              borderColor="brand.500"
+              align="start"
+              spacing={6}
+              position="relative"
+              transition="all 0.3s ease-in-out"
+              backdropFilter="blur(5px)"
+              _hover={{
+                transform: "scale(1.05)",
+              }}
+            >
+              <Box
+                // position="absolute"
+                // top="-12px"
+                // left="50%"
+                // transform="translateX(-50%)"
+                // bg="brand.500"
+                color="white"
+                px={4}
+                py={2}
+                borderRadius="lg"
+                fontSize="sm"
+                backdropFilter="blur(4px)"
+              >
+                Recommended
+              </Box>
+
+              <HStack align="center" justify="center" w="full">
+                <TrophyIcon size={48} color="white" />
+                <Heading
+                  size="lg"
+                  color="white"
+                  flex={1}
+                  textAlign="left"
+                  bgGradient="linear(to-r, brand.100, brand.500)"
+                  bgClip="text"
+                >
+                  Our reward program
+                </Heading>
+              </HStack>
+              <Text color="gray.200">
+                Fast, rewarding, and community-driven
+              </Text>
+
+              <VStack align="start" spacing={4}>
+                <HStack justify="start" align="start" w="full">
+                  <CheckIcon
+                    size={20}
+                    color="var(--anyflow-colors-brand-500)"
+                  />
+                  <VStack align="start" gap={0} flex={1}>
+                    <Text color="gray.100">Lightning fast setup</Text>
+                    <Text color="gray.300" fontSize="sm">
+                      Ready in 5 minutes or less
+                    </Text>
+                  </VStack>
+                </HStack>
+
+                <HStack align="start" justify="start" w="full">
+                  <CheckIcon
+                    size={20}
+                    color="var(--anyflow-colors-brand-500)"
+                  />
+                  <VStack align="start" gap={0} flex={1}>
+                    <Text color="gray.100">Earn while you test</Text>
+                    <Text color="gray.300" fontSize="sm">
+                      Up to $50 + referral bonuses
+                    </Text>
+                  </VStack>
+                </HStack>
+
+                <HStack align="start" justify="start" w="full">
+                  <CheckIcon
+                    size={20}
+                    color="var(--anyflow-colors-brand-500)"
+                  />
+                  <VStack align="start" gap={0} flex={1}>
+                    <Text color="gray.100">Active community</Text>
+                    <Text color="gray.300" fontSize="sm">
+                      20+ developers helping each other
+                    </Text>
+                  </VStack>
+                </HStack>
+
+                <HStack align="start" justify="start" w="full">
+                  <CheckIcon
+                    size={20}
+                    color="var(--anyflow-colors-brand-500)"
+                  />
+                  <VStack align="start" gap={0} flex={1}>
+                    <Text color="gray.100">Multi-chain support</Text>
+                    <Text color="gray.300" fontSize="sm">
+                      12+ chains, one interface
+                    </Text>
+                  </VStack>
+                </HStack>
+              </VStack>
+
+              <Button
+                variant="gradient"
+                size="lg"
+                w="full"
+                rightIcon={<ArrowRightIcon />}
+              >
+                Get Started
+              </Button>
+            </VStack>
+
+            <VStack
+              p={{ base: 6, md: 16 }}
+              borderRadius="lg"
+              border="2px solid"
+              borderColor="gray.600"
+              align="center"
+              justify="center"
+              spacing={{ base: 2, md: 4 }}
+            >
+              <Heading size="md" color="gray.100" textAlign="left" w="full">
+                Manual Testing
+              </Heading>
+              <Text color="danger.400" textAlign="left" w="full">
+                High risk, high cost
+              </Text>
+
+              <VStack align="start" spacing={4} w="full">
+                <HStack justify="start" align="start" w="full">
+                  <XIcon size={20} color="var(--anyflow-colors-danger-500)" />
+                  <VStack align="start" gap={0} flex={1}>
+                    <Text color="gray.300">Error-Prone Process</Text>
+                    <Text color="gray.400" fontSize="sm">
+                      30% deployment failure rate
+                    </Text>
+                  </VStack>
+                </HStack>
+
+                <HStack justify="start" align="start" w="full">
+                  <XIcon size={20} color="var(--anyflow-colors-danger-500)" />
+                  <VStack align="start" gap={0} flex={1}>
+                    <Text color="gray.300">Expensive Testing</Text>
+                    <Text color="gray.400" fontSize="sm">
+                      Wasted gas on failed deploys
+                    </Text>
+                  </VStack>
+                </HStack>
+
+                <HStack justify="start" align="start" w="full">
+                  <XIcon size={20} color="var(--anyflow-colors-danger-500)" />
+                  <VStack align="start" gap={0} flex={1}>
+                    <Text color="gray.300">No Support Network</Text>
+                    <Text color="gray.400" fontSize="sm">
+                      You're on your own
+                    </Text>
+                  </VStack>
+                </HStack>
+
+                <HStack justify="start" align="start" w="full">
+                  <XIcon size={20} color="var(--anyflow-colors-danger-500)" />
+                  <VStack align="start" gap={0} flex={1}>
+                    <Text color="gray.300">Chain Limitations</Text>
+                    <Text color="gray.400" fontSize="sm">
+                      Complex cross-chain setup
+                    </Text>
+                  </VStack>
+                </HStack>
+              </VStack>
+            </VStack>
+          </SimpleGrid>
         </VStack>
       </Container>
 
